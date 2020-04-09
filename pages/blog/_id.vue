497bb237b2f02b32f43606230fc1940dbc89d94d
braisv <brais.vidal.rodriguez@gmail.com>
2020-04-09 11:40:53 +0200

    articles routes

--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="container">
         <h1>Article {{ $route.params.id }} info</h1>
+        <p>{{$route.props}}</p>
     </div>
 </template>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,12 +1,24 @@
 <template>
-    <div class="container">
+    <div class="container mt-5">
         <h1>ARTICLES:</h1>
-        <ul>
-            <li>Article 1</li>
-            <li>Article 2</li>
-            <li>Article 3</li>
-            <li>Article 4</li>
-            <li>Article 5</li>
-        </ul>
+        <div class="row">
+            <div class="col-md-2" v-for="n in articles" :key="n.id"><nuxt-link :to="`/blog/${n.id}`">{{ n.title }}</nuxt-link></div>
+        </div>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            articles: [
+                { id: 1, title: '#1 Title Random', content: 'Random article' },
+                { id: 2, title: '#2 Title Awesome', content: 'Awesome article' },
+                { id: 3, title: '#3 Title Boring', content: 'Boring article' },
+                { id: 4, title: '#4 Title Radiactive', content: 'Radiactive article' },
+                { id: 5, title: '#5 Title Silly', content: 'Silly article' }
+            ]
+        }
+    }
+}
+</script>
